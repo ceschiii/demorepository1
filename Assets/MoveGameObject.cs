d385271fb87b578f979300e256b933d3010dc414
fran <fcl233@nyu.edu>
2020-07-13 19:02:13 -0500

    initial project file push

---
+++ Assets/MoveGameObject.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using UnityEngine;
+
+public class MoveGameObject : MonoBehaviour
+{
+    // The speed that the GameObject will move
+    private float _speed = 2;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += (Vector3.up * _speed) * Time.deltaTime;
+        }
+        // Move the GameObject to the right this frame
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += (Vector3.down * _speed) * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += (Vector3.right * _speed) * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += (Vector3.left * _speed) * Time.deltaTime;
+        }
+
+    }
+}
---
+++ Assets/RotateGameObject.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
+
+public class RotateGameObject : MonoBehaviour
+{
+    //A Vector3 is an array (list) of values, corresponding to X, Y, Z in 3D space
+    private Vector3 _speed = new Vector3 (1, 1, 0);
+
+    
+    //Start is called once at the beginning of runtime
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(_speed);
+    }
+}
